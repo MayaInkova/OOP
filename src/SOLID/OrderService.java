235045852f8b8be6b036e5debb354155d27f770f
Maya <maq002@abv.bg>
2025-01-25 22:57:58 +0200

    SOLID

--- src/SOLID/OrderService.java
+++ src/SOLID/OrderService.java
@@ -2,6 +2,7 @@ package SOLID;
 
 public class OrderService {
     private PaymentMethod paymentMethod;
+    private CreditCardPayment payment = new CreditCardPayment();
 
     public OrderService(PaymentMethod paymentMethod) {
         this.paymentMethod = paymentMethod;
